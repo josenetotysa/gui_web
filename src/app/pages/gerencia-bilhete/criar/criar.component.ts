a75bbe42c6412c39218803654eca2cdf112e0d68
josenetotysa <jose.neto@tysa.com.br>
2025-02-05 11:05:50 -0300

    criar-component

--- src/app/pages/gerencia-bilhete/criar/criar.component.ts
+++ src/app/pages/gerencia-bilhete/criar/criar.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-criar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './criar.component.html',
   styleUrl: './criar.component.scss'
 })
-export class CriarComponent {
+export class CriarComponent implements OnInit {
+  dataMaxima: string = '';
+  quantidadeHoras: string = ''; // Armazena o valor formatado ("02h", "48h")
 
+  ngOnInit() {
+    // Define a data máxima para hoje
+    const hoje = new Date().toISOString().split("T")[0];
+    this.dataMaxima = hoje;
+  }
+
+  formatarHoras() {
+    if (!this.quantidadeHoras) {
+      return;
+    }
+  
+    // Remove qualquer caractere não numérico
+    let horas = this.quantidadeHoras.replace(/\D/g, '');
+  
+    // Se o usuário estiver apagando (removendo o último número), não forçar a reinserção do "h"
+    if (this.quantidadeHoras.endsWith('h') && this.quantidadeHoras.length > horas.length + 1) {
+      this.quantidadeHoras = horas;
+      return;
+    }
+  
+    // Limita a dois caracteres numéricos
+    horas = horas.substring(0, 2);
+  
+    // Se houver algum número, adiciona "h" no final, senão deixa vazio
+    this.quantidadeHoras = horas ? horas + 'h' : '';
+  }
+  
+  
+  
 }
